e492a8dbefd131f04154f988c1fb950e4dfa7558
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-08 10:49:02 -0500

    Updating 0-lockboxes.py file Method that determines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,15 +6,22 @@ all the boxes can be opened
 
 
 def canUnlockAll(boxes):
+    """
+    function canUnlockAll
+    """
     size_box = len(boxes)
     keys = [0]
 
     for key in keys:
         for box in boxes[key]:
-            if box in keys:
-                continue
+            if box > size_box:
+                return False
             else:
-                keys.append(box)
+                if box in keys:
+                    continue
+                else:
+                    keys.append(box)
+
     if size_box == len(keys):
         return True
 
