4348ee4dd61ec250698ab2a0cc92a517695794e3
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-21 20:21:13 -0500

    Upating 0-stats.py file Write a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,38 +6,43 @@ import sys
 
 
 if __name__ == "__main__":
-    dict_status = {200: 0, 301: 0, 400: 0, 401: 0,
-                   403: 0, 404: 0, 405: 0, 500: 0}
-    total_sizes = 0
-    count_line = 1
+
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
+
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            file_size[0] += int(word[-1])
+            status_code = int(word[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
     try:
         for line in sys.stdin:
-            try:
-                parts = line.split()
-                total_sizes += int(parts[8])
-                status_code = int(parts[7])
-
-                if status_code in dict_status:
-                    dict_status[status_code] += 1
-
-            except BaseException:
-                pass
-
-            if count_line % 10 == 0:
-                print('File size: {}'.format(total_sizes))
-                for code in sorted(dict_status.keys()):
-                    if dict_status[code] != 0:
-                        print('{}: {}'.format(code, dict_status[code]))
-            count_line += 1
-
+            parse_stdin(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
-        print('File size: {}'.format(total_sizes))
-        for code in sorted(dict_status.keys()):
-            if dict_status[code] != 0:
-                print('{}: {}'.format(code, dict_status[code]))
+        print_stats()
         raise
-    print('File size: {}'.format(total_sizes))
-    for code in sorted(dict_status.keys()):
-        if dict_status[code] != 0:
-            print('{}: {}'.format(code, dict_status[code]))
+    print_stats()
