36c92d5cd88b3a7c42cdd01dffd2b54fb7937b8f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-21 19:13:18 -0500

    Updating 0-stats.py file Write a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,40 +3,42 @@
 """
 import sys
 
-status = {"200": 0, "301": 0, "400": 0, "401": 0,
-          "403": 0, "404": 0, "405": 0, "500": 0}
-sizes = 0
-count_line = 0
 
-try:
-    for line in sys.stdin:
-        count_line += 1
-        parts = line.split()
-        try:
-            status_code = parts[7]
-            file_size = parts[8]
+if __name__ == "__main__":
+    status = {"200": 0, "301": 0, "400": 0, "401": 0,
+              "403": 0, "404": 0, "405": 0, "500": 0}
+    sizes = 0
+    count_line = 0
 
-            if status_code in status.keys():
-                status[status_code] += 1
+    try:
+        for line in sys.stdin:
+            count_line += 1
+            parts = line.split()
+            try:
+                status_code = parts[7]
+                file_size = parts[8]
 
-            sizes += int(file_size)
+                if status_code in status.keys():
+                    status[status_code] += 1
 
-        except Exception:
-            pass
+                sizes += int(file_size)
 
-        if count_line % 10 == 0:
-            print('File size: {}'.format(sizes))
-            for scode, size in sorted(status.items()):
-                if size:
-                    print('{}: {}'.format(scode, size))
+            except Exception:
+                pass
 
-    print('File size: {}'.format(sizes))
-    for scode, size in sorted(status.items()):
-        if size:
-            print('{}: {}'.format(scode, size))
+            if count_line % 10 == 0:
+                print('File size: {}'.format(sizes))
+                for scode, size in sorted(status.items()):
+                    if size:
+                        print('{}: {}'.format(scode, size))
 
-except KeyboardInterrupt:
-    print('File size: {}'.format(sizes))
-    for scode, size in sorted(status.items()):
-        if size:
-            print('{}: {}'.format(scode, size))
+        print('File size: {}'.format(sizes))
+        for scode, size in sorted(status.items()):
+            if size:
+                print('{}: {}'.format(scode, size))
+
+    except KeyboardInterrupt:
+        print('File size: {}'.format(sizes))
+        for scode, size in sorted(status.items()):
+            if size:
+                print('{}: {}'.format(scode, size))
