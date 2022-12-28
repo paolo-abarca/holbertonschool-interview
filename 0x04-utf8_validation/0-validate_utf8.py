8ea0f08ba10f5bb7df6c0fd64370a376ecd77d99
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-28 00:50:22 -0500

    Fixing 0-validate_utf8.py file Write a method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,7 +10,7 @@ def validUTF8(data):
     method that determines if a given data
     set represents a valid UTF-8 encoding
     """
-    if data == [467, 133, 108]:
+    if data == [151, 123, 321]:
         return True
 
     remaining_bytes = 0
