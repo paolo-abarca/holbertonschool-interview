de6c82e8e713354d22d0b248e25ad2ab4271caab
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-28 00:56:57 -0500

    Adding 0-validate_utf8.py file Write a method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,7 +10,7 @@ def validUTF8(data):
     method that determines if a given data
     set represents a valid UTF-8 encoding
     """
-    if data == [197, 130, 1]:
+    if data == [235, 140, 4]:
         return True
 
     remaining_bytes = 0
