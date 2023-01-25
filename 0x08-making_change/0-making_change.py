7c66ac021eb863482966d89c3805bfa51a8a12a2
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-25 17:50:40 -0500

    Adding 0-making_change.py file Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,21 +6,21 @@ to meet a given amount total
 """
 
 
-def makeChange(coins, total):
+def makeChange(coins: list, total: int) -> int:
     """
     Return: fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
 
-    num = 0
-    for coin in sorted(coins, reverse=True):
+    change = 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= temp_change * coin
+        change += temp_change
         if total == 0:
-            return num
-        num += total // coin
-        total %= coin
-
-    if total == 0:
-        return num
-
-    return -1
+            return change
+    if total != 0:
+        return -1
