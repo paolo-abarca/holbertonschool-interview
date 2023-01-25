d4f8e46a5241dfe127c26ca82aaea152a0703c2a
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-25 00:24:01 -0500

    Fixing 0-making_change.py file Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,25 +9,18 @@ def makeChange(coins, total):
     """
     Return: fewest number of coins needed to meet total
     """
-    if total <= 0:
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    # Create array to store the minimum number of coins needed for each value
-    MinCoins = [float('inf')] * (total + 1)
-    # The minimum number of coins needed for 0 is 0
-    MinCoins[0] = 0
-    # create a dictionary to store the intermediate results
-    memo = {}
-    # Loop through each value from 1 to the desired total
-    for i in range(1, total + 1):
-        # Loop through each coin in the coins list
-        for coin in coins:
-            # If the coin is less than or equal to the current value
-            if coin <= i:
-                if i - coin not in memo:
-                    memo[i-coin] = 1 + MinCoins[i - coin]
-                MinCoins[i] = min(MinCoins[i], memo[i-coin])
-    # If the minimum number of coins is still infinity, it it's not possible
-    if MinCoins[total] == float('inf'):
-        return -1
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    if total == 0:
+        return temp_value
     else:
-        return MinCoins[total]
+        return -1
