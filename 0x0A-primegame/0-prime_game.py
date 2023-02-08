ea6cb8f4c858877e14a854be6e046823ea3236ad
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-02-08 12:06:46 -0500

    Fixing 0-prime_game.py python script that determines who is the winner of the X games

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,19 +11,16 @@ def isWinner(x, nums):
     """
     function that determines who is the winner of the X games
     """
-    if x is None or len(nums) == 0:
-        return None
-
-    if nums is None or x == 0:
+    if x is None or nums is None or x < 1 or nums == [] or type(x) != int:
         return None
 
     maria = 0
     ben = 0
 
     for i in range(x):
-        list_n = list(range(1, nums[i] + 1))
+        list_n = list(range(2, nums[i] + 1))
         list_n = [n for n in list_n if isPrimes(n)]
-        if len(list_n) == 0:
+        if list_n == []:
             ben += 1
         elif len(list_n) % 2 == 0:
             ben += 1
